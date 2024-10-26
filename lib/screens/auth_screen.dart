4fdcb211c77db7bf993e44afa01fff8c63d7a207
Majd-sud <122168340+Majd-sud@users.noreply.github.com>
2024-10-26 20:06:34 +0300

    - Add form fields for username, first name, last name, and phone number in `auth_screen.dart`. - Add validation for the new form fields in `auth_screen.dart`.
    - Update the UI in `profile_screen.dart` to display the user's email and provide a sign out button.

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'profile_screen.dart'; // Import the ProfileScreen
 
 final _firebase = FirebaseAuth.instance;
 final _firestore = FirebaseFirestore.instance;
@@ -17,8 +18,13 @@ class AuthScreen extends StatefulWidget {
 class _AuthScreenState extends State<AuthScreen> {
   final _form = GlobalKey<FormState>();
 
+  var _enteredUsername = '';
+  var _enteredFirstName = '';
+  var _enteredLastName = '';
+  var _enteredPhone = '';
   var _enteredEmail = '';
   var _enteredPassword = '';
+  var _enteredConfirmPassword = '';
   var _isLogin = true;
 
   void _submit() async {
@@ -38,11 +44,16 @@ class _AuthScreenState extends State<AuthScreen> {
           SnackBar(content: Text('Login successful!')),
         );
       } else {
-        await _firebase.createUserWithEmailAndPassword(
+        final userCredential = await _firebase.createUserWithEmailAndPassword(
           email: _enteredEmail,
           password: _enteredPassword,
         );
-        await _firestore.collection('users').doc(_enteredEmail).set({
+
+        await _firestore.collection('users').doc(userCredential.user!.uid).set({
+          'username': _enteredUsername,
+          'firstName': _enteredFirstName,
+          'lastName': _enteredLastName,
+          'phone': _enteredPhone,
           'email': _enteredEmail,
           // Add other user information here
         });
@@ -50,27 +61,16 @@ class _AuthScreenState extends State<AuthScreen> {
           SnackBar(content: Text('Registration successful!')),
         );
       }
-      // Handle successful login or registration (e.g., navigate to another screen)
+      // Navigate to ProfileScreen after successful login or registration
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const ProfileScreen()),
+      );
     } on FirebaseAuthException catch (e) {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text(e.message ?? 'Authentication failed')),
       );
     }
-  }
-
-  Future<List<Map<String, dynamic>>> _retrieveMedications() async {
-    final user = _firebase.currentUser;
-    if (user != null) {
-      final querySnapshot = await _firestore
-          .collection('users')
-          .doc(user.email)
-          .collection('medications')
-          .orderBy('timestamp', descending: true)
-          .get();
-
-      return querySnapshot.docs.map((doc) => doc.data()).toList();
-    }
-    return [];
   }
 
   @override
@@ -85,6 +85,59 @@ class _AuthScreenState extends State<AuthScreen> {
           key: _form,
           child: Column(
             children: [
+              if (!_isLogin)
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'Username'),
+                  onChanged: (value) {
+                    _enteredUsername = value;
+                  },
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your username.';
+                    }
+                    return null;
+                  },
+                ),
+              if (!_isLogin)
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'First Name'),
+                  onChanged: (value) {
+                    _enteredFirstName = value;
+                  },
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your first name.';
+                    }
+                    return null;
+                  },
+                ),
+              if (!_isLogin)
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'Last Name'),
+                  onChanged: (value) {
+                    _enteredLastName = value;
+                  },
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your last name.';
+                    }
+                    return null;
+                  },
+                ),
+              if (!_isLogin)
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'Phone'),
+                  keyboardType: TextInputType.phone,
+                  onChanged: (value) {
+                    _enteredPhone = value;
+                  },
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your phone number.';
+                    }
+                    return null;
+                  },
+                ),
               TextFormField(
                 decoration: const InputDecoration(labelText: 'Email'),
                 keyboardType: TextInputType.emailAddress,
@@ -111,6 +164,23 @@ class _AuthScreenState extends State<AuthScreen> {
                   return null;
                 },
               ),
+              if (!_isLogin)
+                TextFormField(
+                  decoration:
+                      const InputDecoration(labelText: 'Confirm Password'),
+                  obscureText: true,
+                  onChanged: (value) {
+                    _enteredConfirmPassword = value;
+                  },
+                  validator: (value) {
+                    if (value == null ||
+                        value.isEmpty ||
+                        value != _enteredPassword) {
+                      return 'Passwords do not match.';
+                    }
+                    return null;
+                  },
+                ),
               const SizedBox(height: 20),
               ElevatedButton(
                 onPressed: _submit,
@@ -125,15 +195,6 @@ class _AuthScreenState extends State<AuthScreen> {
                 child: Text(_isLogin
                     ? 'Create new account'
                     : 'I already have an account'),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () async {
-                  final medications = await _retrieveMedications();
-                  // Handle the retrieved medications (e.g., display them in a list)
-                  print(medications);
-                },
-                child: const Text('Retrieve Medications'),
               ),
             ],
           ),
--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -6,13 +6,30 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final user = FirebaseAuth.instance.currentUser;
+
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Profile'),
+      ),
       body: Center(
-        child: IconButton(
-          onPressed: () async {
-            await FirebaseAuth.instance.signOut();
-          },
-          icon: const Icon(Icons.exit_to_app),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (user != null) ...[
+              Text('Email: ${user.email}'),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () async {
+                  await FirebaseAuth.instance.signOut();
+                  Navigator.pushReplacementNamed(context, '/auth');
+                },
+                child: const Text('Sign Out'),
+              ),
+            ] else ...[
+              const Text('No user is signed in'),
+            ],
+          ],
         ),
       ),
     );
